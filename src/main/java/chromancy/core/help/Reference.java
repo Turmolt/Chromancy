ebf66c962e4afdd0de09268798a3e257f485b7d3
Sam Gates <skiracer1292@gmail.com>
2015-07-09 13:27:07 -0400

    Version 0.1

--- src/main/java/chromancy/core/help/Reference.java
+++ src/main/java/chromancy/core/help/Reference.java
@@ -3,5 +3,5 @@ package chromancy.core.help;
 public class Reference {
 
 	public static final String MODID = "chromancy";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "0.1";
 }
