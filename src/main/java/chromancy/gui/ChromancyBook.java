f0a22025a20be4b3990b9e87565b9ff244abf258
WhiskeyGolf <richards19k@yahoo.com>
2015-07-21 18:58:35 -0400

    Gui Update Book

--- src/main/java/chromancy/gui/ChromancyBook.java
+++ src/main/java/chromancy/gui/ChromancyBook.java
@@ -10,25 +10,27 @@ import net.minecraftforge.client.event.sound.PlaySoundEvent;
 
 public class ChromancyBook extends GuiScreen {
 	
-	String image = "textures/gui/ChromancyBook_Light.png";
+	String image = "textures/gui/ChromancyBook_01.png";
 	
 	boolean DarkLight = true;
 	
 	int guiWidth = 256;
-	int guiHeight = 173;
+	int guiHeight = 158;
 
-	int tabWidth = 22;
-	int tabHeight = 18;
+	int tabWidth = 20;
+	int tabHeight = 20;
 	
-	int tab1v = 176;
-	int tab2v = 176;
-	int tab3v = 176;
-	int tab4v = 176;
-	int tab5v = 176;
-	int tab6v = 176;
-	int tab7v = 176;
-	int tab8v = 176;
-	int tab9v = 176;
+	int tab1v = 160;
+	int tab2v = 160;
+	int tab3v = 160;
+	int tab4v = 160;
+	int tab5v = 160;
+	int tab6v = 160;
+	int tab7v = 160;
+	int tab8v = 160;
+	int tab9v = 160;
+	int tab10v = 160;
+	int tab11v = 160;
 	
 	int Clicked = 0;
 	
@@ -42,127 +44,139 @@ public class ChromancyBook extends GuiScreen {
 		int guiY = (height - guiHeight) / 2;
 		switch(Clicked){
 		case 1:
-			tab1v = 194;
-			tab2v = 176;
-			tab3v = 176;
-			tab4v = 176;
-			tab5v = 176;
-			tab6v = 176;
-			tab7v = 176;
-			tab8v = 176;
-			tab9v = 176;
-			image = "textures/gui/ChromancyBook_Light.png";
+			tab1v = 180;
+			tab2v = 160;
+			tab3v = 160;
+			tab4v = 160;
+			tab5v = 160;
+			tab6v = 160;
+			tab7v = 160;
+			tab8v = 160;
+			tab9v = 160;
+			image = "textures/gui/ChromancyBook_01.png";
 			break;
 		case 2:
-			tab1v = 176;
-			tab2v = 194;
-			tab3v = 176;
-			tab4v = 176;
-			tab5v = 176;
-			tab6v = 176;
-			tab7v = 176;
-			tab8v = 176;
-			tab9v = 176;
-			image = "textures/gui/ChromancyBook_Red.png";
+			tab1v = 160;
+			tab2v = 180;
+			tab3v = 160;
+			tab4v = 160;
+			tab5v = 160;
+			tab6v = 160;
+			tab7v = 160;
+			tab8v = 160;
+			tab9v = 160;
+			image = "textures/gui/ChromancyBook_01.png";
 			break;
 		case 3:
-			tab1v = 176;
-			tab2v = 176;
-			tab3v = 194;
-			tab4v = 176;
-			tab5v = 176;
-			tab6v = 176;
-			tab7v = 176;
-			tab8v = 176;
-			tab9v = 176;
-			image = "textures/gui/ChromancyBook_Orange.png";
+			tab1v = 160;
+			tab2v = 160;
+			tab3v = 180;
+			tab4v = 160;
+			tab5v = 160;
+			tab6v = 160;
+			tab7v = 160;
+			tab8v = 160;
+			tab9v = 160;
+			image = "textures/gui/ChromancyBook_01.png";
 			break;
 		case 4:
-			tab1v = 176;
-			tab2v = 176;
-			tab3v = 176;
-			tab4v = 194;
-			tab5v = 176;
-			tab6v = 176;
-			tab7v = 176;
-			tab8v = 176;
-			tab9v = 176;
-			image = "textures/gui/ChromancyBook_Yellow.png";
+			tab1v = 160;
+			tab2v = 160;
+			tab3v = 160;
+			tab4v = 180;
+			tab5v = 160;
+			tab6v = 160;
+			tab7v = 160;
+			tab8v = 160;
+			tab9v = 160;
+			image = "textures/gui/ChromancyBook_01.png";
 			break;
 		case 5:
-			tab1v = 176;
-			tab2v = 176;
-			tab3v = 176;
-			tab4v = 176;
-			tab5v = 194;
-			tab6v = 176;
-			tab7v = 176;
-			tab8v = 176;
-			tab9v = 176;
-			image = "textures/gui/ChromancyBook_Green.png";
+			tab1v = 160;
+			tab2v = 160;
+			tab3v = 160;
+			tab4v = 160;
+			tab5v = 180;
+			tab6v = 160;
+			tab7v = 160;
+			tab8v = 160;
+			tab9v = 160;
+			image = "textures/gui/ChromancyBook_01.png";
 			break;
 		case 6:
-			tab1v = 176;
-			tab2v = 176;
-			tab3v = 176;
-			tab4v = 176;
-			tab5v = 176;
-			tab6v = 194;
-			tab7v = 176;
-			tab8v = 176;
-			tab9v = 176;
-			image = "textures/gui/ChromancyBook_Blue.png";
+			tab1v = 160;
+			tab2v = 160;
+			tab3v = 160;
+			tab4v = 160;
+			tab5v = 160;
+			tab6v = 180;
+			tab7v = 160;
+			tab8v = 160;
+			tab9v = 160;
+			image = "textures/gui/ChromancyBook_01.png";
 			break;
 		case 7:
-			tab1v = 176;
-			tab2v = 176;
-			tab3v = 176;
-			tab4v = 176;
-			tab5v = 176;
-			tab6v = 176;
-			tab7v = 194;
-			tab8v = 176;
-			tab9v = 176;
-			image = "textures/gui/ChromancyBook_Indigo.png";
+			tab1v = 160;
+			tab2v = 160;
+			tab3v = 160;
+			tab4v = 160;
+			tab5v = 160;
+			tab6v = 160;
+			tab7v = 180;
+			tab8v = 160;
+			tab9v = 160;
+			image = "textures/gui/ChromancyBook_01.png";
 			break;
 		case 8:
-			tab1v = 176;
-			tab2v = 176;
-			tab3v = 176;
-			tab4v = 176;
-			tab5v = 176;
-			tab6v = 176;
-			tab7v = 176;
-			tab8v = 194;
-			tab9v = 176;
-			image = "textures/gui/ChromancyBook_Violet.png";
+			tab1v = 160;
+			tab2v = 160;
+			tab3v = 160;
+			tab4v = 160;
+			tab5v = 160;
+			tab6v = 160;
+			tab7v = 160;
+			tab8v = 180;
+			tab9v = 160;
+			image = "textures/gui/ChromancyBook_01.png";
 			break;
 		case 9:
-			tab1v = 176;
-			tab2v = 176;
-			tab3v = 176;
-			tab4v = 176;
-			tab5v = 176;
-			tab6v = 176;
-			tab7v = 176;
-			tab8v = 176;
-			tab9v = 194;
-			image = "textures/gui/ChromancyBook_Black.png";
+			tab1v = 160;
+			tab2v = 160;
+			tab3v = 160;
+			tab4v = 160;
+			tab5v = 160;
+			tab6v = 160;
+			tab7v = 160;
+			tab8v = 160;
+			tab9v = 180;
+			image = "textures/gui/ChromancyBook_01.png";
+			break;
+		case 10:
+			tab10v = 180;
+			tab11v = 160;
+			image = "textures/gui/ChromancyBook_01.png";
+			break;
+		case 11:
+			tab10v = 160;
+			tab11v = 180;
+			image = "textures/gui/ChromancyBook_01.png";
 			break;
 			
 		}
 		mc.renderEngine.bindTexture(new ResourceLocation(Reference.MODID, image));
 		drawTexturedModalRect(guiX, guiY, 0, 0, guiWidth, guiHeight);
-		drawTexturedModalRect(guiX + 12, guiY + 18, 0, tab1v, tabWidth, tabHeight);
-		drawTexturedModalRect(guiX + 12, guiY + 18 + tabHeight + 1, 21, tab2v, tabWidth, tabHeight);
-		drawTexturedModalRect(guiX + 12, guiY + 18 + (tabHeight * 2) + 2, 42, tab3v, tabWidth, tabHeight);
-		drawTexturedModalRect(guiX + 12, guiY + 18 + (tabHeight * 3) + 3, 63, tab4v, tabWidth, tabHeight);
-		drawTexturedModalRect(guiX + 12, guiY + 18 + (tabHeight * 4) + 4, 84, tab5v, tabWidth, tabHeight);
-		drawTexturedModalRect(guiX + 12, guiY + 18 + (tabHeight * 5) + 5, 105, tab6v, tabWidth, tabHeight);
-		drawTexturedModalRect(guiX + 12, guiY + 18 + (tabHeight * 6) + 6, 126, tab7v, tabWidth, tabHeight);
-		drawTexturedModalRect(guiX + 12, guiY + 18 + (tabHeight * 7) + 7, 147, tab8v, tabWidth, tabHeight);
+		drawTexturedModalRect(guiX, guiY, 0, tab1v, tabWidth, tabHeight);
+		drawTexturedModalRect(guiX, guiY + tabHeight + 1, 20, tab2v, tabWidth, tabHeight);
+		drawTexturedModalRect(guiX, guiY + (tabHeight * 2) + 2, 40, tab3v, tabWidth, tabHeight);
+		drawTexturedModalRect(guiX, guiY + (tabHeight * 3) + 3, 60, tab4v, tabWidth, tabHeight);
+		drawTexturedModalRect(guiX, guiY + (tabHeight * 4) + 4, 80, tab5v, tabWidth, tabHeight);
+		drawTexturedModalRect(guiX, guiY + (tabHeight * 5) + 5, 100, tab6v, tabWidth, tabHeight);
+		drawTexturedModalRect(guiX, guiY + (tabHeight * 6) + 6, 120, tab7v, tabWidth, tabHeight);
+		drawTexturedModalRect(guiX, guiY + (tabHeight * 7) + 7, 140, tab8v, tabWidth, tabHeight);
+		drawTexturedModalRect(guiX + 240, guiY, 180, tab10v, tabWidth, tabHeight);
+		drawTexturedModalRect(guiX + 240, guiY + tabHeight + 1, 200, tab11v, tabWidth, tabHeight);
 		if(DarkLight){
-			drawTexturedModalRect(guiX + 240, guiY + 18 + (tabHeight * 7) + 7, 168, tab9v, tabWidth, tabHeight);
+			drawTexturedModalRect(guiX + 240, guiY + (tabHeight * 7) + 7, 160, tab9v, tabWidth, tabHeight);
 		}
 		//fontRendererObj.drawString("Chromancy", guiX + 10, guiY + 10, 0xFFFFFF);
 		
@@ -199,32 +213,39 @@ public class ChromancyBook extends GuiScreen {
 		int guiX = (width - guiWidth) / 2;
 		int guiY = (height - guiHeight) / 2;
 		
-		if(guiX + 12 < x && x < guiX + 12 + tabWidth && guiY + tabHeight < y && y < guiY + tabHeight * 2){
+		if(guiX < x && x < guiX + tabWidth && guiY < y && y < guiY + tabHeight){
 			Clicked = 1;
+			System.out.println("Button 1");
 		}
-		if(guiX + 12 < x && x < guiX + 12 + tabWidth && guiY + tabHeight * 2 < y && y < guiY + tabHeight * 3){
+		if(guiX < x && x < guiX + tabWidth && guiY + tabHeight < y && y < guiY + tabHeight * 2){
 			Clicked = 2;
 		}
-		if(guiX + 12 < x && x < guiX + 12 + tabWidth && guiY + tabHeight * 3 < y && y < guiY + tabHeight * 4){
+		if(guiX < x && x < guiX  + tabWidth && guiY + tabHeight * 2 < y && y < guiY + tabHeight * 3){
 			Clicked = 3;
 		}
-		if(guiX + 12 < x && x < guiX + 12 + tabWidth && guiY + tabHeight * 4 < y && y < guiY + tabHeight * 5){
+		if(guiX < x && x < guiX + tabWidth && guiY + tabHeight * 3 < y && y < guiY + tabHeight * 4){
 			Clicked = 4;
 		}
-		if(guiX + 12 < x && x < guiX + 12 + tabWidth && guiY + tabHeight * 5 < y && y < guiY + tabHeight * 6){
+		if(guiX< x && x < guiX + tabWidth && guiY + tabHeight * 4 < y && y < guiY + tabHeight * 5){
 			Clicked = 5;
 		}
-		if(guiX + 12 < x && x < guiX + 12 + tabWidth && guiY + tabHeight * 6 < y && y < guiY + tabHeight * 7){
+		if(guiX < x && x < guiX + tabWidth && guiY + tabHeight * 5 < y && y < guiY + tabHeight * 6){
 			Clicked = 6;
 		}
-		if(guiX + 12 < x && x < guiX + 12 + tabWidth && guiY + tabHeight * 7 < y && y < guiY + tabHeight * 8){
+		if(guiX < x && x < guiX + tabWidth && guiY + tabHeight * 6 < y && y < guiY + tabHeight * 7){
 			Clicked = 7;
 		}
-		if(guiX + 12 < x && x < guiX + 12 + tabWidth && guiY + tabHeight * 8 < y && y < guiY + tabHeight * 9){
+		if(guiX < x && x < guiX + tabWidth && guiY + tabHeight * 7 < y && y < guiY + tabHeight * 8){
 			Clicked = 8;
 		}
-		if(guiX + 240 < x && x < guiX + 240 + tabWidth && guiY + tabHeight * 8 < y && y < guiY + tabHeight * 9 && DarkLight){
+		if(guiX + 240 < x && x < guiX + 240 + tabWidth && guiY + tabHeight * 7 < y && y < guiY + tabHeight * 8 && DarkLight){
 			Clicked = 9;
+		}
+		if(guiX + 240 < x && x < guiX + 240 + tabWidth && guiY< y && y < guiY  + tabHeight){
+			Clicked = 10;
+		}
+		if(guiX + 240 < x && x < guiX + 240 + tabWidth && guiY + tabHeight < y && y < guiY + tabHeight * 2){
+			Clicked = 11;
 		}
 		
 	}
